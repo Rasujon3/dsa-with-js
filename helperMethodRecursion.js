44e17fbdc88660dcf73341070ba28a307875cac7
Rasujon3 <rasujon3@gmail.com>
2022-07-29 02:43:08 +0600

    recursion helper method

---
+++ helperMethodRecursion.js
@@ -0,0 +1,14 @@
+function finOddNumber(arr) {
+  let result = [];
+  function helper(input) {
+    if (input.length === 0) return;
+    if (input[0] % 2 !== 0) {
+      result.push(input[0]);
+    }
+    helper(input.slice(1));
+  }
+  helper(arr);
+  return result.length;
+}
+
+console.log(finOddNumber([2, 3, 5, 6, 3, 4, 12, 32, 55, 75]));
